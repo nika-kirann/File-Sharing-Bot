5472c2e69eef0eda010a19b0f7854f4b15cc0ca7
Kiran xer <kiranser074@gmail.com>
2025-08-06 16:58:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,14 +20,14 @@ API_HASH = os.environ.get("API_HASH", "05907d9dd5b68cd478769b1e1b841f97")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7891602366"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kevinvg006:N800YtS25a4rRSW5@cluster0.skyk86r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
